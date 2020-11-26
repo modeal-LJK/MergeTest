aa1d3dedcf98b071183241f670b8c955e50cf3e6
modeal-LJK <jinguk2018@gmail.com>
2020-11-26 15:57:12 +0900

    미사용

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php
-Header("Location:/_sys_/page/main.php");
-Header("Location:/_sys_/page/main.php");
-Header("Location:/_sys_/page/main.php");
-
-Header("Location:/_sys_/page/main.php");
-?>
+미사용
